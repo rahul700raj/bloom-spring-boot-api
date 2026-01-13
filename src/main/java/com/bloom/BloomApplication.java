a52ab6ddd83c9a611d71a22686e52c340f46f40b
rahul700raj <rm2778643@gmail.com>
2026-01-13 17:34:15 +0530

    Add main Spring Boot application class

---
+++ src/main/java/com/bloom/BloomApplication.java
@@ -0,0 +1,11 @@
+package com.bloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BloomApplication.class, args);
+    }
+}
